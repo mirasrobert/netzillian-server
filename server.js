25ca46da0a8155324419caf1045332875b45ecc2
mirasrobert <mrmirasrobert@gmail.com>
2022-05-18 09:10:21 +0800

    Add Origin

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.use(cors(corsOptions))
 */
 app.use(cors())
 
+app.options('/*', function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  res.end()
+})
+
 // Connect database
 ;(async function connectDB() {
   try {
